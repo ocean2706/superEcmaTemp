cf7af7f120690be0cc14f90ffcae2375e84e809d
ocean2706 <george.bungarzescu@gmail.com>
2013-05-16 02:00:07 +0300

    rev 2

---
+++ jscompiler/MigrationTool/CmdLineUtils.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+
+namespace MigrationTool
+{
+	public class CmdLineUtils
+	{
+		public CmdLineUtils ()
+		{
+		}
+		public static String GetArgument(String name){
+			System.Collections.Generic.List<String> lines=new List<String>(Environment.GetCommandLineArgs());
+			String val = "";
+			lines.ForEach (delegate(String l) {
+				if(l.ToLower().StartsWith(name.ToLower()+"=")){
+					val= l.ToLower().Replace(name.ToLower()+"=").Trim ('"').Trim ("'");
+				}else if(l.ToLower()==name.ToLower()){
+					val="True";
+				}
+			});
+		}
+	}
+}
+
