d92178512d672144cbccbc30ec2cf8ca7b799dbb
ocean2706 <george.bungarzescu@gmail.com>
2013-05-29 00:15:14 +0200

    refactor2 tools

--- jscompiler/tools/ReplaceInJavaFile/ReplaceItem.cs
+++ jscompiler/tools/ReplaceInJavaFile/ReplaceItem.cs
@@ -9,19 +9,46 @@ namespace ReplaceInJavaFile
 
     public class CommonReplaceDelegate
     {
+		/// <summary>
+		/// default delegate for replacement
+		/// </summary>
+		/// <param name="incomming">Incomming.</param>
+		/// <param name="r">The red component.</param>
         public static String Replace1(String incomming,ReplaceListItem r)
         {
 
 
-            if(!r.IsRegexp && incomming.ToLower().Contains(r.ToReplaceInMatch)){
-                return incomming.Replace(r.ToReplaceInMatch,r.Replacement); // simple replacement
+            if((!r.IsRegexp) && (incomming.ToLower().Contains(r.ToReplaceInMatch.ToLower()))){
+               incomming= incomming.Replace(r.ToReplaceInMatch,r.ReplaceWith); // simple replacement
+				incomming=incomming.Replace(r.ToReplaceInMatch.ToLower(),r.ReplaceWith);
+				return incomming;
             }
             String ret = incomming;
-            MatchCollection foundList = Regex.Matches(incomming, search);
+            MatchCollection foundList = Regex.Matches(ret, r.Regexp);
+			List<String> replaced=new List<String>();
+			if (!r.IsPrefixSuffix) {
             foreach (Match m in foundList)
             {
-                String toReplace = m.Value.Replace(iSearch, replacement);
-                ret = ret.Replace(m.Value, toReplace);
+					if(!replaced.Contains(m.Value.Trim ())){
+					// replace in founded string 
+					String toReplace = m.Value.Replace (r.ToReplaceInMatch, r.ReplaceWith);
+					ret = ret.Replace (m.Value, toReplace);
+						replaced.Add(m.Value);
+					}
+				}
+			}
+			else {
+
+				// in this seq we just add prefix and suffix to the original string..so it can mach multiple times...
+				//
+				//this must be fixed
+				foreach (Match m in foundList)
+				{
+					if (!replaced.Contains (m.Value.Trim ())) {
+						ret = r.Prefix + m.Value + r.Suffix;
+						replaced.Add(m.Value);
+					}
+				}
             }
             return ret;
         }
@@ -30,7 +57,7 @@ namespace ReplaceInJavaFile
         public ReplaceCallback ExecuteCallBack = null;
         public String Regexp { get; set; }
         public String ToReplaceInMatch { get; set; }
-        public String Replacement { get; set; }
+        public String ReplaceWith { get; set; }
 
         public String Prefix { get; set; }
         public String Suffix { get; set; }
